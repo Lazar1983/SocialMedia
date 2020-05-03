ecafefb1311bebbaf2e3a011a0e22140e959814e
Lazar <lazov_lazar@yahoo.com>
2020-05-03 21:36:53 +0200

    delete scream

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -2,7 +2,8 @@ import {
   SET_SCREAMS,
   LIKE_SCREAM,
   UNLIKE_SCREAM,
-  LOADING_DATA
+  LOADING_DATA,
+  DELETE_SCREAM
 } from '../types';
 
 const initialState = {
@@ -31,6 +32,12 @@ export default function(state = initialState, action) {
       return {
         ...state
       };
+    case DELETE_SCREAM:
+      index = state.screams.findIndex((scream) => scream.screamId === action.payload);
+      state.screams.splice(index, 1);
+      return {
+        ...state
+      }
     default:
       return state;
   }
